65037a2a194017199acc643f4fea59c6d7d21299
podestal <luisrodriguezpodesta@gmail.com>
2023-11-16 13:00:06 -0500

    filter

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,24 @@
+from typing import Any
 from django.db.models.aggregates import Count
 from django.contrib import admin
+from django.db.models.query import QuerySet
 from django.utils.html import format_html, urlencode
 from django.urls import reverse
 from . import models
+
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory' 
+    parameter_name = 'inventory'
+
+    def lookups(self, request , model_admin):
+        return [
+            ('<10', 'Low')
+        ]
+    
+    def queryset(self, request, queryset: QuerySet):
+        if self.value() == '<10':
+            queryset.filter(inventory__lt=10)
+
 
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
@@ -13,6 +29,7 @@ class OrderAdmin(admin.ModelAdmin):
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
+    list_filter = ['collection', 'last_update', InventoryFilter]
     list_per_page = 10
     list_select_related = ['collection']
 
