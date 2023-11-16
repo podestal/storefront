4f15ddd0acd05aa2a9c05da8b32a74fd2eb29223
podestal <luisrodriguezpodesta@gmail.com>
2023-11-16 13:36:58 -0500

    updating forms

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,6 @@
 from typing import Any
 from django.db.models.aggregates import Count
+from django.contrib.contenttypes.admin import GenericTabularInline
 from django.contrib import admin
 from django.db.models.query import QuerySet
 from django.utils.html import format_html, urlencode
@@ -25,8 +26,18 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'placed_at', 'customer']
     list_select_related = ['customer']
 
+class TagInline(GenericTabularInline):
+    # model = TaggedItem
+    pass
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['collection']
+    prepopulated_fields = {
+        'slug': ['title']
+    }
+    actions = ['clear_inventory']
+    # fields, exclude and readonly_fields are quite useful
     list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_filter = ['collection', 'last_update', InventoryFilter]
@@ -41,6 +52,14 @@ class ProductAdmin(admin.ModelAdmin):
         if product.inventory < 10:
             return 'Low'
         return 'Ok'
+    
+    @admin.action(description='Clear inventory')
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(
+            request,
+            f'{updated_count} products were succesfully updated'
+        )
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
@@ -53,6 +72,7 @@ class CustomerAdmin(admin.ModelAdmin):
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ['title', 'products_count']
+    search_fields = ['title']
 
     @admin.display(ordering='products_count')
     def products_count(self, collection):
